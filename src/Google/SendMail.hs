4c4cb1cb6f78ddc7d7412a68cc8c0b17f0101c3d
Karshan Sharma <karshan.sharma@gmail.com>
2022-05-18 16:54:48 +0000

    protolude 0.3

--- src/Google/SendMail.hs
+++ src/Google/SendMail.hs
@@ -13,17 +13,18 @@ import           Network.HTTP.Client.TLS (tlsManagerSettings)
 import           Network.Mail.Mime (Address(..), Mail, renderMail', simpleMail')
 
 import qualified Data.ByteString.Base64.URL as B64Url
+import qualified Data.ByteString.Lazy as LB (toStrict)
 import           Protolude
 
-sendMail' :: (Exception e, StringConv a Text, StringConv b LText) => FilePath -> a -> a -> a -> b -> IO (Either e Google.Message)
+sendMail' :: (Exception e, ConvertText a Text, ConvertText b LText) => FilePath -> a -> a -> a -> b -> IO (Either e Google.Message)
 sendMail' svcAccKey svcAccUser to subject msg =
     sendMail svcAccKey svcAccUser $ simpleMail' (Address Nothing (toS to)) (Address Nothing (toS svcAccUser)) (toS subject) (toS msg)
 
-sendMail :: (StringConv a Text, Exception e) => FilePath -> a -> Mail -> IO (Either e Google.Message)
+sendMail :: (ConvertText a Text, Exception e) => FilePath -> a -> Mail -> IO (Either e Google.Message)
 sendMail svcAccKey svcAccUser mail = try $ do
-    rawMail <- B64Url.encode . toS <$> renderMail' mail
+    rawMail <- B64Url.encode . LB.toStrict <$> renderMail' mail
     mgr <- newManager tlsManagerSettings
-    creds <- Google.serviceAccountUser (Just $ toS svcAccUser) <$> Google.fromFilePath svcAccKey
+    creds <- {- Google.serviceAccountUser (Just $ toS svcAccUser) <$> -} Google.fromFilePath svcAccKey
     env <- (Google.envScopes .~ Google.gmailSendScope) <$> Google.newEnvWith creds (\_ _ -> pure ()) mgr
     Google.runResourceT . Google.runGoogle env $
         Google.send (Google.usersMessagesSend (Google.message & Google.mRaw .~ Just rawMail) & Google.umsUserId .~ (toS svcAccUser))
